9b5dd03ee14df524ed2d648c2b2cd7ee311250c3
Juan Torres <juand@Juans-MacBook-Pro.local>
2017-03-21 11:16:52 -0600

    Proyecto base.

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Juan de Dios Torres on 21/03/17.
+//  Copyright © 2017 Juan de Dios Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
